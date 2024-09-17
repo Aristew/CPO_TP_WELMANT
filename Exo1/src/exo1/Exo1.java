627d102f04c5573811eaccac3cd1a161abe2c7ae
Ariste Welmant <ariste.welmant@epfedu.fr>
2024-09-17 16:24:06 +0200

    Création d'un nouveau projet versionné

--- Exo1/src/exo1/Exo1.java
+++ Exo1/src/exo1/Exo1.java
@@ -1,12 +1,12 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package exo1;
 
+import java.util.Scanner;
+
 /**
- *
- * @author arist
+ * TP0 exercice 1
+ * @date 17/09/2024
+ * @author ariste welmant
  */
 public class Exo1 {
 
@@ -14,7 +14,15 @@ public class Exo1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        System.out.println("Bonjour");
+        System.out.println("Aurevoir");
+        String prenom;
+        Scanner sc;
+        sc = new Scanner(System.in);
+        System.out.println("Bonjour, quel est votre prenom ?");
+        prenom = sc.nextLine();
+        System.out.println("Votre prenom est "+prenom+"");
     }
     
 }
