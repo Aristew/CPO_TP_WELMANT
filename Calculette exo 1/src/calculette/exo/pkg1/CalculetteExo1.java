c4197523de9ba3c9f50b67d73347eb4542507804
Ariste Welmant <ariste.welmant@epfedu.fr>
2024-09-17 17:13:46 +0200

    Création d'un nouveau projet versionné

---
+++ Calculette exo 1/src/calculette/exo/pkg1/CalculetteExo1.java
@@ -0,0 +1,49 @@
+
+package calculette.exo.pkg1;
+
+/**
+ * Une calculette
+ * @date 17/09/2024
+ * @author Welmant Ariste
+ */
+
+import java.util.Scanner;
+
+public class CalculetteExo1 {
+
+
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("""
+                           
+                            Please enter the operator : \n1) add \n2) substract \n3) multiply \n4) divide \n5) modulo""");
+        var sc = new Scanner(System.in);
+        int operator;
+        operator = sc.nextInt();
+        System.out.println("Please enter the first operand:");
+        double operand1 = sc.nextDouble();
+        System.out.println("Please enter the second operand:");
+        double operand2 = sc.nextDouble();
+        double result = 0;
+        if (1==operator) { 
+            result = operand1 + operand2;
+        }
+        if (2==operator)  { 
+            result = operand1 - operand2;
+        }
+        if (3==operator)  { 
+            result = operand1 * operand2;
+        }
+        if (4==operator)  { 
+            result = operand1 / operand2;
+        }
+        if (5==operator)  { 
+            result = operand1 % operand2;
+        }
+    }
+}            
+    
+
