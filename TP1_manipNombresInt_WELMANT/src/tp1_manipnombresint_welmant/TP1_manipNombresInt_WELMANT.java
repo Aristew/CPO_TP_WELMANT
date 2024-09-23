3808d0e2cf8066f9c0874507aac8aa2f2a831a15
Ariste Welmant <ariste.welmant@epfedu.fr>
2024-09-23 10:09:59 +0200

    Création d'un nouveau projet versionné

---
+++ TP1_manipNombresInt_WELMANT/src/tp1_manipnombresint_welmant/TP1_manipNombresInt_WELMANT.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp1_manipnombresint_welmant;
+
+/**
+ * @date 23/09/2024
+ * @author ariste welmant
+ * 
+ */
+public class TP1_manipNombresInt_WELMANT {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Saisir 2 entiers");
+        
+        // TODO code application logic here
+    }
+    
+}
