d7ab9a0611ef6238f8ce45c55a69dea57c8cb49c
Ariste Welmant <ariste.welmant@epfedu.fr>
2024-09-23 10:21:08 +0200

    Création d'un nouveau projet versionné

--- TP1_manipNombresInt_WELMANT/src/tp1_manipnombresint_welmant/TP1_manipNombresInt_WELMANT.java
+++ TP1_manipNombresInt_WELMANT/src/tp1_manipnombresint_welmant/TP1_manipNombresInt_WELMANT.java
@@ -1,8 +1,10 @@
-/*
+/*;
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package tp1_manipnombresint_welmant;
+
+import java.util.Scanner;
 
 /**
  * @date 23/09/2024
@@ -16,7 +18,14 @@ public class TP1_manipNombresInt_WELMANT {
      */
     public static void main(String[] args) {
         System.out.println("Saisir 2 entiers");
-        
+        var sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int difference = a - b;
+        int produit = a*b;
+        System.out.println("Voici la somme: "+ a+b); 
+        System.out.println("Voici la difference: "+ difference);
+        System.out.println("Voici le produit: "+ produit);
         // TODO code application logic here
     }
     
