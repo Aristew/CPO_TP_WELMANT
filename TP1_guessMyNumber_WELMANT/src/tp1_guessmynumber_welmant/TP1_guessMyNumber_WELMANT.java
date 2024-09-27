fe1b746357f172f0eca45afd71da733362361adc
Ariste Welmant <ariste.welmant@epfedu.fr>
2024-09-27 14:18:15 +0200

    Création d'un nouveau projet versionné

--- TP1_guessMyNumber_WELMANT/src/tp1_guessmynumber_welmant/TP1_guessMyNumber_WELMANT.java
+++ TP1_guessMyNumber_WELMANT/src/tp1_guessmynumber_welmant/TP1_guessMyNumber_WELMANT.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package tp1_guessmynumber_welmant;
 
 import java.util.Random;
@@ -17,38 +13,93 @@ public class TP1_guessMyNumber_WELMANT {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // Générateur de nombre aléatoire
         Random generateurAleat = new Random();
-        int n;
-        int i = 0;
-        int compteur = 0;
-        var sc = new Scanner(System.in);
-        n = generateurAleat.nextInt(100);
-        System.out.println("Choissisez la difficulté : \n 1) Facile \n 2) Normal \n 3) Difficile ");
-        int z = sc.nextInt();
-        switch (z){
-            case -> 1 {
-                while (i<1){
-                    compteur += 1;
+        int n; // Le nombre à deviner
+        int compteur = 0; // Compteur de tentatives
+        var sc = new Scanner(System.in); // Scanner pour lire les entrées utilisateur
+
+        n = generateurAleat.nextInt(100); // Génère un nombre aléatoire entre 0 et 99
+        System.out.println("Choisissez la difficulté : \n 1) Facile \n 2) Normal \n 3) Difficile ");
+        int z = sc.nextInt(); // Lire le choix de la difficulté
+
+        switch (z) {
+            case 1 -> { // Mode Facile
+                System.out.println("Mode Facile : Vous avez un nombre illimité de tentatives.");
+                boolean trouve = false;
+                while (!trouve) {
+                    compteur++;
                     System.out.println("Saisir un nombre entier entre 0 et 100");
                     int a = sc.nextInt();
-                    if (a == n ){
-                        System.out.println("gagne");
-                        i += 1;
-                    }
-                    else if (a < n){
-                    System.out.println("trop petit");
-                    }
-                    else if (a>n){
-                    System.out.println("Trop grand");
-                    }
-                    }
-                    System.out.print("Nombre de tentatives = "+ compteur);
-            
-}
-    
-}
 
-}
-    
+                    if (a == n) {
+                        System.out.println("Bravo, vous avez gagné !");
+                        trouve = true;
+                    } else if (a < n) {
+                        if (n - a > 20) {
+                            System.out.println("Vraiment trop petit");
+                        } else {
+                            System.out.println("Trop petit");
+                        }
+                    } else {
+                        if (a - n > 20) {
+                            System.out.println("Vraiment trop grand");
+                        } else {
+                            System.out.println("Trop grand");
+                        }
+                    }
+                }
+                System.out.print("Nombre de tentatives = " + compteur);
+            }
+            case 2 -> { // Mode Normal
+                System.out.println("Mode Normal : Vous avez 10 tentatives pour deviner le nombre.");
+                boolean trouve = false;
+                while (!trouve && compteur < 10) {
+                    compteur++;
+                    System.out.println("Saisir un nombre entier entre 0 et 100");
+                    int a = sc.nextInt();
 
+                    if (a == n) {
+                        System.out.println("Bravo, vous avez gagné !");
+                        trouve = true;
+                    } else if (a < n) {
+                        System.out.println("Trop petit");
+                    } else {
+                        System.out.println("Trop grand");
+                    }
+                }
+                if (!trouve) {
+                    System.out.println("Vous avez épuisé vos 10 tentatives. Le nombre était : " + n);
+                } else {
+                    System.out.println("Nombre de tentatives = " + compteur);
+                }
+            }
+            case 3 -> { // Mode Difficile
+                System.out.println("Mode Difficile : Vous avez seulement 5 tentatives pour deviner le nombre.");
+                boolean trouve = false;
+                while (!trouve && compteur < 5) {
+                    compteur++;
+                    System.out.println("Saisir un nombre entier entre 0 et 100");
+                    int a = sc.nextInt();
+
+                    if (a == n) {
+                        System.out.println("Bravo, vous avez gagné !");
+                        trouve = true;
+                    } else if (a < n) {
+                        System.out.println("Trop Petit");
+                    } else {
+                        System.out.println("Trop Grand");
+                    }
+                }
+                if (!trouve) {
+                    System.out.println("Vous avez épuisé vos 5 tentatives. Le nombre était : " + n);
+                } else {
+                    System.out.println("Nombre de tentatives = " + compteur);
+                }
+            }
+            default -> {
+                System.out.println("Choix de difficulté invalide. Veuillez relancer le programme.");
+            }
+        }
+    }
+}
