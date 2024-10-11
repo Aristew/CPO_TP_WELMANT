c7f9711eae95732d3be43957ff10e19dde8b569c
Ariste Welmant <ariste.welmant@epfedu.fr>
2024-10-11 11:56:27 +0200

    Création d'un nouveau projet versionné

---
+++ TP2_Bieres_WELMANT/src/BouteilleBiere.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Welmant Ariste
+ */
+public class BouteilleBiere {
+    String nom;
+    float degreAlcool;
+    String brasserie;
+    boolean Ouverte;
+    public void lireEtiquette() {
+        System.out.println("Bouteille de " + nom +" (" + degreAlcool + " degres) \nBrasserie : " + brasserie ) ;
+}
+    
+}
